26a2ffe3398e15bccdbda64fe49128da0a82d895
Nino van de Wiel <info@nimoxdesign.nl>
2025-10-06 07:35:27 +0200

    feat: enum file for item distinction

---
+++ php/src/Enum/ItemEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose\Enum;
+
+enum ItemEnum: string
+{
+    case AGED_BRIE = 'Aged Brie';
+    case BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    case CONJURED = 'Conjured Mana Cake';
+    case SULFURAS = 'Sulfuras, Hand of Ragnaros';
+}
